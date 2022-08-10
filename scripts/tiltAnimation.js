a43097b0245496c4d1cc2f604999331f07b21a70
Uday Kumar Kamalapuram <uday@udays-mbp.myfiosgateway.com>
2022-08-10 12:23:31 -0400

    Tilt Animation Changes
     '#' will be ignored, and an empty message aborts the commit.

--- scripts/tiltAnimation.js
+++ scripts/tiltAnimation.js
@@ -1,4 +1,4 @@
-import VanillaTilt from "vanilla-tilt";
+import VanillaTilt from "./vanilla-tilt.js";
 
 export default function initTiltAnimation() {
   const elements = document.querySelectorAll(".js-tilt");
